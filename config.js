c75c2dc6a2d66c8248c4804da6fac201fbb45529
Elżbieta Kukułka <ellakaras@gmail.com>
2018-05-12 13:05:58 +0200

    Ready task 17_7 - add Google login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '615838752051-c6a6j8o312dpjfs111bfmsn1ajfok627.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'lTR1MlEFdnz_R3-BwUYHnxnp',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
